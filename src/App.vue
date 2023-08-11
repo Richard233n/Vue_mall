67e48f706e5637a9c61df2f2dc3840b6cdb07601
Richard阿辰 <1943126663@qq.com>
2023-08-11 20:31:04 +0800

    编写地区区域布局

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
 <template>
+    <div>
+        <div class="wrapper">
+            <div class="position">
+                <span class="iconfont position_icon">&#xe61a;</span>
+                Richard工程实验室
+                <span class="iconfont position_notice">&#xe66d;</span>
+            </div>
+        </div>
+    </div>
+
+
     <div class="docker">
         <div class="docker_item docker_item--active">
             <div class="iconfont">&#xe67d;</div>
@@ -24,6 +35,52 @@
 </script>
 
 <style lang="scss">
+@import './style//variables.scss';
+@import './style/mixins.scss';
+
+
+// 头部样式
+.wrapper {
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: .5rem;
+    right: 0;
+    padding: 0 .18rem;
+
+}
+
+.position {
+    position: relative;
+    padding: .16rem .24rem .16rem 0;
+    line-height: .22rem;
+    font-size: .16rem;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+
+    .position_icon {
+        position: relative;
+        top: .01rem;
+        font-size: .2rem;
+    }
+
+    .position_notice {
+        position: absolute;
+        right: 0;
+        top: .17rem;
+        font-size: .2rem;
+    }
+
+    color: $content-fontcolor;
+    font-family: 'Fira Code';
+}
+
+
+
+
+
+// 底部栏样式
 .docker {
     display: flex;
     box-sizing: border-box;
